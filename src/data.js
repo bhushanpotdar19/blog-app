218708a4f485f8a2c6e8c635ee3cd641b245af7e
bhushanpotdar19 <bhushanpotdar3@gmail.com>
2024-06-21 23:25:16 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,92 @@
+const blogs = [
+    {
+        id: "Make your own tune or tune it-001",
+        title: "Make your own tune or tune it",
+        content: "For those of you who are playing with AI technologies in personal life or at work, you would have come across a word called “Fine tuning”. In the fast-evolving world of artificial intelligence, most companies are facing a critical decision: should they invest in creating a custom AI model from scratch or opt for a more affordable approach by fine-tuning an existing model? While custom models offer bespoke solutions, they come with hefty price price tags. Let’s take the example of ChatGPT or GPT 4.0 which everyone is talking about. It costs $5.00 / 1million tokens for input and $15.00 / 1million token for output. To put this in perspective, if you have a use case where you chat with an application using this APIs and roughly have 10,000 users having standard conversation of 100 prompt input / 100 output tokens (which is literally few lines!) a day, you are talking about almost $20 / day range of expenses which translates to almost $7,500 per year. But are models easy to fine tune and can you get the desired result? If so, how?",
+        author: {
+            name: "Kartik Sharma",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2022/11/Kartik-1.png",
+        },
+        date: "June 21, 2024",
+        categories: ["AI Musings" , "Tech" , "TOI"],
+    },
+    {
+        id: "When to ignore and believe the AI hype cycle -002",
+        title: "When to ignore and believe the AI hype cycle",
+        content: "Picture this: It’s 2002. You’re lucky enough to get your hands on a first-of-its-kind smartphone that lets you message anyone in the world. Life changing, right? In the early 2000s, BlackBerry, Nokia and Ericsson were among the companies dominating the cellphone market. Fast forward to 2007, and the debut of the iPhone changed everything and eliminated the previous market leaders.The iPhone revolution teaches us that the earliest innovators during a tech hype cycle don’t always emerge as the long-term winners. In fact, most often they do not. As the AI hype cycle continues to ebb and flow and early-stage generative AI startups sit at lofty valuations, this is a crucial consideration for all founders and VCs.",
+        author: {
+            name: "Samir Kumar",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2024/06/samir-kumar-150x150.jpeg",
+        },
+        date: "June 20, 2024",
+        categories: ["Voices" , "India", "Tech "],
+    },
+    {
+        id: "AI: The key to unlocking new software companies-003",
+        title: "AI: The key to unlocking new software companies",
+        content: "The software industry is on the cusp of a revolution, thanks to Artificial Intelligence (AI). While AI’s potential is widely recognized, its impact on software business models is still largely untapped. Traditionally, software has focused on enabling existing work, but AI-enabled software can deliver outcomes, transforming the way businesses operate.The dominant SaaS business model is currently based on a per-seat model, limiting growth potential. However, AI-powered software can deliver specific business outcomes, unconstrained by employee usage. This shift unlocks new markets, enabling software to reach small businesses, under-resourced teams, and new geographies.The implications are significant. AI will expand Total Addressable Markets (TAM) in various categories, enabling niche categories to expand and large categories to grow further. New monetization variables will emerge, as businesses will pay for outcomes rather than just software.",
+        author: {
+            name: "Rajiv Dabhadkar",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/backup/rajivdprofile.jpg",
+        },
+        date: "June 17, 2024",
+        categories: ["Green Carrot" , "Business", "Tech" , "TOI"],
+    },
+    {
+        id: "Assigning mobile phone numbers is not costless-004",
+        title: "Assigning mobile phone numbers is not costless",
+        content: "Trai’s proposal to levy a charge on phone numbers it allocates to telecom companies should go through. Pointing out that phone numbers are an “exceedingly valuable public resource which is not infinite”, the regulator is giving telcos the option to pass on the cost to consumers. Telecom has been one of India’s success stories. As of March 2024, there were as many as 1.16bn mobile subscribers in India, next only to China. But, as a Trai consultation paper has highlighted, just over 19% of the total numbers (219mn) fall under the ‘service suspended pending disconnection’ category.System has its limits | Telcos are holding on to phone numbers with low or zero utilisation, fearing a loss of user base. With SIMs cheap to procure, it is common for consumers to own more numbers than they need. Forgotten amidst this waste of resources, is the reality that there’s only so much demand the telecom system can meet. India has a 10-digit series number system. This imposes a limit on the number of connections that can be allotted. For a mobile number is more than just a number. It is formatted in a certain way that syncs with the larger system. And moving to a different series is not easy and would entail problems and costs. It is not for nothing that telcos/consumers are charged for mobile connections in a host of countries around the world.Demand will only rise | In an age of hyper-connectivity, the pressure on telecom’s system capacity can only grow, whether it is from IOT and M2M, or cars and gadgets with chips. There are other costs this imposes on the system. Authorities have a responsibility to keep tabs on how connections are used, especially given the rise in cyber crimes. Charging for phone numbers is particularly justified for ‘vanity numbers’ telcos auction at high prices. The quantum of the charge Trai has proposed can always be worked out – a ‘nominal’ fee is what it has in mind. But putting a price on the resource would ensure both telcos and consumers value it more.",
+        author: {
+            name: "TOI Edit",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2019/01/toiedit-logo.jpg",
+        },
+        date: "June 14, 2024",
+        categories: ["TOI Editorials" ,"Edit Page", "Tech" , "TOI"],
+    },
+    {
+        id: "Generative AI presentation for beginners (45 minutes)-005",
+        title: "Generative AI presentation for beginners (45 minutes) ",
+        content: "This column did not post an article last week for the first time since 15 November last year. I would have loved a no-week-missed year and was working towards that. I was glad to watch kid’s videos with my son that teach how it is quite possible for things to go against the plan. I love the 2019 Bollywood movie Chhichhore, in which Anni tries to throw the ball into the basketball goal, but it bounces back from the hoop. They lose the match. H3 Hostel won the game. But they cheered H4 Hostel for their team spirit and perseverance. A less than perfect ending. Quite frequent and relatable. On lines like the above, I might not be able to give you the best (or the only) outline for a 45-minute presentation on Generative AI. You decide. First, The Generative AI Revolution. If the Generative AI hype is based on truth, is it still hype? Talk about my convoluted thoughts. Generative AI generates content in the form of text, audio, music, images, and videos. It has applications in every function and every industry. The impact is in how we work, write our job descriptions and the expectations from a role in an organisation. There is a myriad of use cases to explore. The list of use cases is limited only by our imagination.Second, Insights into the Creation of the Training Dataset. The training dataset for building an LLM is created by taking each word of a part of the sentence as an input variable and the subsequent word as the output variable. The model generates by predicting the next word. In a standard multiclass classification ML problem, the word with the highest probability of occurrence should be picked. However, selecting the topmost word every time would cause the loss of creativity of LLMs. Hence, quite often, we go further down.",
+        author: {
+            name: "Malaya Rout",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2024/06/damo-copy-150x150.jpeg",
+        },
+        date: "June 10, 2024",
+        categories: ["DataScienceVibes" , "Tech" , "TOI"],
+    },
+    {
+        id: "To Anyone Who Has Lost Themselves-006",
+        title: "To Anyone Who Has Lost Themselves",
+        content: "Love— be it romantic, self, or otherwise — is rascally.A few undiligent weeks and you can lose yourself, parts of you breaking off in pieces as the sun rises and sets.This is how love breaks. Not in one swift motion, but tiny fracture by tiny fracture until it amounts to a crash. We don not see it coming until it is screeching at us, almost too late, giving only a small warning to change course.You can fall out of love with your partner this way. You can fall out of love with yourself this way. You can fall out of love with your life this way. With your friends. With your family. With your routines. With your passion. With your work. Time has a way of slipping off, never feeling too late until it is. And, love is a speeding train — no matter the form love takes — and if you do not hold onto it, if you do not watch yourself in it, it can leave you behind, breathless, uncertain, lost. We can’t let days speed past without giving them thought, because this is the way we become lost. This is how we run in circles, because we have not stopped long enough to find a direction. Our life can run away with or without us attached to it and, while it seems we are the driver and conductor of whatever exists in our lives, anyone with life experience knows this is not the truth. Life moves on without us.Sometimes love is so swift in its transformations, in its growth, that if we do not pay attention we could miss it, charging ahead down a path that is leading us further from where we want to be going. I do this thing when life gets difficult where I abandon myself.Like I shed the part of myself that needs cultivation and go into survival mode, go into Safe Mode with limited capability, limited access.To stop loving myself is to come home to what I remember, what I know, what feels familiar. To cave into myself — introspective and self-analyzed and caught in the purgatory between thinking and doing — is the safest I ever feel.",
+        author: {
+            name: "Jamie Varon",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2017/01/Untitled-6-150x150.jpg",
+        },
+        date: "Jan 5, 2023",
+        categories: ["Breaking Shackles" , "Lifestyle", "Tech"],
+    },
+    {
+        id: "No, Microsoft isn’t calling you:-007",
+        title: "No, Microsoft isn’t calling you:",
+        content: "In the digital age, as we become increasingly reliant on technology, tech support scams have emerged as a pervasive threat. These scams exploit our dependence on computers and smartphones, tricking unsuspecting users into parting with their money or personal information. Tech support scams typically involve fraudsters posing as technical support representatives from well-known companies like Microsoft, Apple, or Google. These scammers contact victims through various means, including phone calls, pop-up messages, emails, or even social media. They claim that the victim’s device is infected with a virus, running slow, or experiencing other issues that need immediate attention. Once the victim is convinced of the supposed problem, the scammer may request remote access to their device, trick them into installing malicious software, or ask for payment for unnecessary services. The end goal is to steal sensitive information, install malware, or extort money.",
+        author: {
+            name: "Rajvardhan Oak",
+            avtar: "https://i.pravatar.cc/150?img=68",
+        },
+        date: "June 3, 2024",
+        categories: ["Cyber Chronicles" , "Tech" , "TOI"],
+    },
+    {
+        id: "When installing packages took longer than coding-008",
+        title: "When installing packages took longer than coding",
+        content: "This week, I experimented with building a CNN (Convolutional Neural Network) image classification model. I wanted to give GitHub Copilot a try. GitHub Copilot is an AI pair programmer that helps with code completions. One can ask the tool to “explain” a segment of code, fix an error, or generate a Python docstring. Python docstring is a sentence documenting what the module, class, or method does.Before I started coding, installing the required Python packages took me about forty minutes. The actual coding consisted of fifty lines of code and took less than ten minutes. A few things stood out.I got comment-completion recommendations from the editor when I started typing. The recommendations were not without substance. For example, after the block of code that fits the CNN model, the suggestion was to evaluate and then save the model. This might be a simple suggestion for experienced data scientists. However, think about someone learning to build an ML model for the first time. This is non-trivial. Immediately after I accepted the tool’s suggestion to complete the comment line, it suggested the content of the whole function. That was another nine lines of code. It intelligently picked up the function name, variable names, keywords, and parameters. Only the parameter values were out of place for obvious reasons. The tool did not have details of the input images, which were placed in my system’s local directory. However, it was easy to change the values given that I already had the syntactically correct function written by the tool. ",
+        author: {
+            name: "Malaya Rout",
+            avtar: "https://static.toiimg.com/imagenext/toiblogs/photo/blogs/wp-content/uploads/2023/11/Malaya-Rout-150x150.png",
+        },
+        date: "June 1, 2024",
+        categories: ["DataScienceVibes" , "Tech" , "TOI"],
+    }
+]
+
+export default blogs
